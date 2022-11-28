6b514428a16e60f866a361827cd9fa6d203e90de
Shreyash Kerekar <78008383+Shreyash-ux@users.noreply.github.com>
2022-11-28 17:14:46 +0530

    Create main.tf

---
+++ hello/main.tf
@@ -0,0 +1,17 @@
+provider "google" {
+  project = var.project_name
+  region  = var.region_name
+}
+resource "google_service_account" "service_account" {
+  account_id   = "service-hello-test-account-id"
+  display_name = "Hello Test Service Account"
+}
+resource "google_compute_network" "hello_test_vpc_network" {
+  name = "hello-vpc-network"
+}
+resource "google_compute_subnetwork" "public-subnetwork" {
+  name          = "hello-terraform-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.hello_test_vpc_network.name
+}
